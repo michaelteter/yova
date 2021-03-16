1ea035a42386c16a0c8d9ce35eb93b4bb84e72a6
Michael Teter <m@michaelteter.com>
2021-03-16 17:36:45 +0100

    chore: added screenshots for demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   include ActionController::HttpAuthentication::Token
 
   def authenticate_user!
+    # For easier dev testing, uncomment this auth bypass below.
+    # return true
+
     enc_token, _options = token_and_options(request)
     user_uuid = AuthenticationService.validate(enc_token)
 
